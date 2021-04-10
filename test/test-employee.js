6af885c848f0a12f72aac70e569954fb32d7c972
Pikachu <jouxiong15@gmail.com>
2021-04-10 10:57:55 -0500

    finished employee test

---
+++ test/test-employee.js
@@ -0,0 +1,56 @@
+const managerQ = require("../questions/manager-questions.js");
+const Manager = require("../library/Manager.js");
+const engineerQ = require("./questions/engineer-questions.js");
+const Engineer = require("../library/Engineer.js");
+const internQ = require("../questions/intern-questions.js");
+const Intern = require("../library/Intern.js");
+const Employee = require("../library/Employee.js");
+const { TestScheduler } = require("@jest/core"); // WHAT IS THIS?
+
+// Employee.js Test
+test("Can create Employee object", () => {
+    const e = new Employee();
+    expect(typeof(e)).toBe("object");
+})
+
+test("Can set name via constructor arguments", () => {
+    const name = "Tony";
+    const e = new Employee(name);
+    expect(e.name).toBe(name);
+})
+
+test("Can set ID via constructor arguments", () => {
+    const id = "123";
+    const e = new Employee("Name", id);
+    expect(e.id).toBe(id);
+})
+
+test("Can set email via constructor arguments", () => {
+    const email = "tony123@gmail.com";
+    const e = new Employee("Name", "ID", email);
+    expect(e.email).toBe(email);
+})
+
+test("Can get name via getName()", () => {
+    const testName = "Tony";
+    const e = new Employee(testName);
+    expect(e.getName()).toBe(testName);
+})
+
+test("Can get id via getID()", () => {
+    const testID = "123";
+    const e = new Employee("Tony", testID);
+    expect(e.getID()).toBe(testID);
+})
+
+test("Can get email via getEmail()", () => {
+    const testEmail = "tony123@gmail.com";
+    const e = new Employee("Tony", "123", testEmail);
+    expect(e.getEmail()).toBe(testEmail);
+})
+
+test("getRole() should return 'Employee'", () => {
+    const testRole = "Employee";
+    const e = new Employee("Tony", "123", "tony123@gmail.com");
+    expect(e.getRole()).toBe(testRole);
+})
